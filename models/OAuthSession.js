02b413aa9b57aa23a10768d61777a07757cff160
bitcoin4cashqc <bitcoin4cashqc@hotmail.com>
2024-11-08 18:30:13 -0800

    added a comment field

--- models/OAuthSession.js
+++ models/OAuthSession.js
@@ -5,6 +5,7 @@ const OAuthSessionSchema = new mongoose.Schema({
   oauth_token: { type: String, required: true },
   oauth_token_secret: { type: String, required: true },
   taskId: { type: String, required: true },
+  comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, expires: '10m' }, // Expires after 10 minutes
 });
 
